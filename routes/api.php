99c09333459df57c0179b37523ccb0957b082f7e
João Victor Sobroza Dal Ross <joaovictor0908.jv@gmail.com>
2026-05-29 12:21:03 -0300

    Ativando as rotas de put e delete

--- routes/api.php
+++ routes/api.php
@@ -7,5 +7,5 @@ use App\Http\Controllers\ApiController;
 Route::get('refeicoes', [ApiController::class, 'getRefeicoes']);
 Route::get('refeicoes/{data}', [ApiController::class, 'getRefeicoesByData']);
 Route::post('refeicoes', [ApiController::class, 'postRefeicoes']);
-//Route::put('refeicoes/{id}', [ApiController::class, 'updateRefeicao']);
-//Route::delete('refeicoes/{id}', [ApiController::class, 'deleteRefeicao']);
+Route::put('refeicoes/{id}', [ApiController::class, 'putRefeicoes']);
+Route::delete('refeicoes/{id}', [ApiController::class, 'deleteRefeicoes']);
